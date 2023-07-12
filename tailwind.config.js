ec0920e224addc903f762d873ca56f40464c7bef
gsngdavid <gsngdavid@gmail.com>
2023-07-12 12:54:29 +0200

    Finish navigation and houses section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "red-primary": "#ff385c",
+        "red-secondary": "#ff385c",
+        "gray-primary": "#b0b0b0",
+        "gray-secondary": "#dddddd",
+        "white-secondary": "#ffffffe6",
+        "black-secondary": "#222222",
+      },
+      fontFamily: {
+        "roboto": "'Roboto', sans-serif;"
+      }
+    },
   },
   plugins: [],
 }
